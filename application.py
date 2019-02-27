b4aacfda4e7f96f5e6fc1e0a39a7d1993f59c758
jason162020 <mydream162020@gmail.com>
2019-02-27 20:57:40 +0530

    'first'

--- application.py
+++ application.py
@@ -24,29 +24,34 @@ Session(app)
 
 # Set up database
 #engine = create_engine(os.getenv("DATABASE_URL"))
+# using Heroku url directly
 engine = create_engine("postgres://iorynocojkwjgc:d6104889e6eeee320700f04b6012980b77506087bded1c13ed5e9d155c97a245@ec2-107-22-162-8.compute-1.amazonaws.com:5432/d22b8flhk40kgi")
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+	
 	if request.method=='POST':
 		user =request.form.get('username')
 		pswrd=request.form.get('passwrd')
 
-		
+		#avoiding whitespaces problem
 		if user!="" and pswrd!="":
 			try:
 				usr=db.execute("SELECT * FROM users WHERE username=:user AND pswrd=:pswrd",{'user':user,'pswrd':pswrd}).fetchone()
 				
 				if usr.username== user:
-					return render_template('search.html',user=usr)
+					session["user"]=usr.username
+					session['user_id']=usr.id
+					return render_template('search.html')
 				else: 
-					return 'Invalid username or passwrd'
+					return render_template("index.html",message="Invalid username or password")
 			except Exception as e:
-				return 'Invalid input'
+
+				return render_template("index.html",message='Invalid username or password')
 		else:
-			return 'Invalid username or password'
+			return render_template("index.html",message='Invalid username or password')
 	else:
 		return render_template('index.html')
 
@@ -62,37 +67,59 @@ def register():
 		
 
 		try:
-			db.execute("INSERT INTO users (username,pswrd) VALUES(:username,:pswrd)",{'username':username,'pswrd':pswrd})
-			db.commit()
-			return "Account has been"
+			if username.strip()!="" and pswrd.strip()!="":
+				db.execute("INSERT INTO users (username,pswrd) VALUES(:username,:pswrd)",{'username':username,'pswrd':pswrd})
+				db.commit()
+				return "Account has been created"
+			else:
+				return render_template("register.html",message="invalid input")
 		except Exception as e:
-			return "Invalid username or password"
+			return render_template("register.html",message="User name already exists")
 		
 @app.route("/search",methods=['GET','POST'])
 def search():
 	name=request.form.get('srch')
 	keyword=request.form.get('keyword')
 	if name=='isbn':
+		# searching by isbn and also using LIKE in SQL
 		lists=db.execute("SELECT * FROM books WHERE isbn LIKE :isbn",{'isbn':'%'+keyword+'%'}).fetchall()
 	elif name=='title':
+		# searching by title name and also using LIKE in SQL
 		lists=db.execute("SELECT * FROM books WHERE title LIKE :title",{'title':'%'+keyword+'%'}).fetchall()
 	elif name=='author':
+		# searching by author name and also using LIKE in SQL
 		lists=db.execute("SELECT * FROM books WHERE author LIKE :author",{'author':'%'+keyword+'%'}).fetchall()
 
 	if request.method=='GET':
 		return render_template('search.html')
 	else:
+		
 		return render_template('search.html',lists=lists)
 
-@app.route('/book_details/<int:book_id>')
+@app.route('/book_details/<int:book_id>',methods=['GET','POST'])
 def book_details(book_id):
+	message=""  # message after submitting the review
 	details=db.execute("SELECT * FROM books WHERE id=:book_id",{'book_id':book_id}).fetchone()
 	res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "BKMKb4vaIpvUbGUseF2Yg", "isbns":details.isbn })
 	data=res.json()
 	avg_ratings=data["books"][0]['average_rating']
 	num_ratings=data["books"][0]['work_ratings_count']
 	#return jsonify(data["books"][0]['average_rating'])
-	return render_template('book_details.html',avg_ratings=avg_ratings,num_ratings=num_ratings,details=details)
+	if request.method=='POST':
+		user_id=session["user_id"]
+		isbn=details.isbn
+		ratings=int(request.form.get("rating"))
+		comment=request.form.get("comment")
+		try:
+			db.execute("INSERT INTO reviews(rating,comment,isbn,user_id)VALUES(:rating,:comment,:isbn,:user_id)",{'rating':ratings,'comment':comment.strip(),'isbn':isbn,'user_id':user_id})
+			db.commit()
+			message="reviews have been submitted"
+		except Exception as e:
+			message=str(e) # in case of eny error
+
+	
+	all_reviews=db.execute("SELECT reviews.rating,reviews.comment,users.username FROM reviews INNER JOIN users ON reviews.user_id=users.id WHERE reviews.isbn=:isbn",{'isbn':details.isbn}).fetchall()
+	return render_template('book_details.html',avg_ratings=avg_ratings,num_ratings=num_ratings,details=details,message=message,all_reviews=all_reviews)
 
 
 
